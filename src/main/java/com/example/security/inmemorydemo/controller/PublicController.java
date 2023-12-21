f7a47f3ff14df079938d6e4fb3f5d61dadc212f7
Busra Kaya <busrakaya0697@hotmail.com>
2023-12-21 15:24:39 +0300

    Fix api

--- src/main/java/com/example/security/inmemorydemo/controller/PublicController.java
+++ src/main/java/com/example/security/inmemorydemo/controller/PublicController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/public")
 public class PublicController {
-    @GetMapping("/hello")
+    @GetMapping()
     public String sayHelloFromPublic(){
         return "Hello from /public method";
     }
